aa22907900542c00b6faec9f95f35ac888bdaf8d
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-31 11:40:59 +0000

    refactor test_extract.py

--- test/test_extract.py
+++ test/test_extract.py
@@ -3,7 +3,8 @@ from src.extract import lambda_handler
 
 def test_extracts_currency_list_with_eur_as_base_currency():
     result = lambda_handler(None, None)
-    assert isinstance(result, dict)
-    assert "date" in result
-    assert "eur" in result
-    assert result["date"] == "2024-10-31"
+    if result:
+        assert isinstance(result, dict)
+        assert "date" in result
+        assert "eur" in result
+        assert result["date"] == "2024-10-31"
